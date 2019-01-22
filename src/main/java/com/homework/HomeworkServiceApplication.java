8132396f373ee9a27a4ba0a0d016ef4b26f0f2df
Artem Yakovlev <yakovlev.artem29@gmail.com>
2019-01-22 15:12:01 +0200

    Springboot: add barebones of the app

---
+++ src/main/java/com/homework/HomeworkServiceApplication.java
@@ -0,0 +1,14 @@
+package com.homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkServiceApplication.class, args);
+	}
+
+}
+
